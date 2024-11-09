6f32b6bdc929e7f03c3589064e1f580044ff95d5
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2024-11-09 23:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+() => { 
+    cy.get('button').should('have.attr', 'id', 'submit'); 
+    cy.get('button').should('have.attr', 'type', 'submit'); 
+    cy.get('button').should('contain', 'Submit');
+}
